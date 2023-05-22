bea6d0a11d112bb1a6f094befacef0789892a990
Sebasmdl <sebmarti@bancolombia.com.co>
2023-05-21 23:08:08 -0500

    uploading modules

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-page',
+  templateUrl: './basic-page.component.html',
+  styles: [
+  ]
+})
+export class BasicPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-page',
+  templateUrl: './dynamic-page.component.html',
+  styles: [
+  ]
+})
+export class DynamicPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
